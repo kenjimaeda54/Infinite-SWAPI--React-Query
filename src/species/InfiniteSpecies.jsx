dc9b434edcae1c31afa19ac8cd345884b46b0376
kenjimaeda54 <kenjimaedfamily@gmail.com>
2022-03-25 08:07:25 -0300

    fix: finished api

--- src/species/InfiniteSpecies.jsx
+++ src/species/InfiniteSpecies.jsx
@@ -1,3 +1,4 @@
+import { useInfiniteQuery } from "react-query";
 import InfiniteScroll from "react-infinite-scroller";
 import { Species } from "./Species";
 
@@ -8,6 +9,31 @@ const fetchUrl = async (url) => {
 };
 
 export function InfiniteSpecies() {
-  // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const { hasNextPage, isLoading, fetchNextPage, data, error, isFetching } =
+    useInfiniteQuery(
+      "species",
+      ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+      {
+        getNextPageParam: (lastPageData) => lastPageData.next || undefined,
+      }
+    );
+  if (isLoading) return <div className="loading">Loading...</div>;
+  if (error) return <div className="error">{error}</div>;
+
+  return (
+    <InfiniteScroll hasMore={hasNextPage} loadMore={fetchNextPage}>
+      {data.pages.map((speciesData) => {
+        const random = parseFloat(Math.random() * 1000000);
+        return speciesData.results.map((specie) => (
+          <Species
+            key={`${specie.name}-${random}-${specie.language}`}
+            name={specie.name}
+            language={specie.language}
+            averageLifespan={specie.average_lifespan}
+          />
+        ));
+      })}
+      {isFetching && <div className="loading">Loading...</div>}
+    </InfiniteScroll>
+  );
 }
